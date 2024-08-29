a05228be56def83687c27485fc15032de146ef70
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2024-08-29 18:52:24 -0300

    chore: Update express config for handling larger JSON

--- index.ts
+++ index.ts
@@ -4,9 +4,10 @@ import { router } from './api/routes/routes.js';
 import { connectDb } from './api/config/db.js';
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 app.use(router);
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 const port = 3333;
 connectDb();
 
